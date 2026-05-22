320e26f70ff3c914c86073af4bb0c90ff7baeae2
eram711 <eram38513@gmail.com>
2026-05-21 18:34:44 -0600

    Add ESLint configuration, linting workflow, and example file

---
+++ src/example.js
@@ -0,0 +1,9 @@
+// Intentional linting violations to demonstrate the lint workflow
+var unusedVariable = "I am unused and use double quotes"
+const message = "Hello, world!"
+
+function greet(name) {
+    console.log("Hi " + name)
+}
+
+greet("ESLint")
